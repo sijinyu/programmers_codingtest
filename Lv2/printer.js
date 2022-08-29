08060ea213750650cf9e99310a5657cdb05b56ab
sijinyu <sijinyudev@gmail.com>
2022-08-29 21:50:09 +0900

    feat LV2 printer

---
+++ Lv2/printer.js
@@ -0,0 +1,13 @@
+function solution(priorities, location) {
+  t cnt = 0;;
+while ( priorities.length) {if (priorities[0] < Math.max(...priorities)) {
+   i ( --location < 0) location = priorities.length - 1;
+    prti es.push(priorities.shift());;
+ } els;
+    t++;
+     ilocat;ion < 0) return cnt;
+   priti es.shift();;
+};
+  }
+
+
